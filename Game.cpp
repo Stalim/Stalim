16e4d14133763bf2336c62de252539dc6232f486
Dean Banh <32099851+deanbanh@users.noreply.github.com>
2018-04-01 22:21:52 -0400

    Add files via upload
    Game umbrella class framework done, header file is all good, Game.cpp has blank implementations for all methods (as outlined in Milestone #2), but the Main class is incomplete. We are currently working on it.

---
+++ Game.cpp
@@ -0,0 +1,87 @@
+//
+// Created by Dean Banh on 3/27/2018.
+//
+
+#include "Game.h";
+
+using namespace std;
+
+//Implement Game.h's methods
+
+//Constructor
+void Game::Game() {
+
+
+}
+
+void Game::printMainMenu() {
+
+
+}
+
+void Game::readRules() {
+
+
+}
+
+void Game::startBlinds() {
+
+
+}
+
+void Game::startPreFlop() {
+
+
+}
+
+void Game::startFlop() {
+
+
+}
+
+void Game::startTurn() {
+
+
+}
+
+void Game::startRiver() {
+
+
+}
+
+void Game::startShowdown() {
+
+
+}
+
+void Game::displayPlayerStats() {
+
+
+}
+
+void Game::playerAction() {
+
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+int main(){
+
+        //Switch case statements, make room for Preethi/Paige's intro, flow of the game and the turns (no output)
+    //Bad input checks
+
+
+
+
+
+
+
+
+
+
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ Game.h
@@ -0,0 +1,52 @@
+//
+// Created by bgtra on 3/27/2018.
+//
+
+#ifndef POKERGAME_GAME_H
+#define POKERGAME_GAME_H
+
+#include <iostream>;
+
+using namespace std;
+
+class Game {
+
+private:
+
+    int gameNumber;
+    int numActivePlayers;
+    int player;
+    int potMoney;
+    int bigBlind;
+    int smallBlind;
+    int playerTurn;
+    int betRound;
+    int currentCallAmount;
+    int currentRaiseAmount; //Relevance?!? Redundant?
+    //Cards communityCards[];
+
+public:
+
+    void Game();
+    void printMainMenu();
+    void readRules();
+    void startBlinds();
+    void startPreFlop();
+    void startFlop();
+    void startTurn();
+    void startRiver();
+    void startShowdown();
+    void displayPlayerStats();
+    void playerAction(); //What is this?
+
+
+
+};
+
+
+
+
+
+
+
+#endif //POKERGAME_GAME_H
