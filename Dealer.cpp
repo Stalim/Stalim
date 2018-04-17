99955d0d246a19c86b0c9069edaf98002a067f03
higgy300 <jhigueraballesteros@gmail.com>
2018-04-17 16:41:23 -0400

    fixed a small error on the deconstructor

--- Dealer.cpp
+++ Dealer.cpp
@@ -27,7 +27,7 @@ Dealer::Dealer()
 Dealer::~Dealer()
 {
 	// This will deallocate the memory used for the array once main ends.
-	for(int i = 0; i < 2; i++) {
+	for(int i = 0; i < 13; i++) {
 		delete[] deck[i];
 	}
 	delete[] deck;
