93c5575c6184e18103a0f8df685a1c4bd74b5061
dospina3 <dospina3@ufl.edu>
2018-04-18 16:38:20 -0400

    added getters and setters. Need to be implemented

--- Game.h
+++ Game.h
@@ -13,33 +13,51 @@ class Game {
 
 private:
 
-    int gameNumber;
-    int numActivePlayers;
-    int player;
-    int potMoney;
-    int bigBlind;
-    int smallBlind;
-    int playerTurn;
-    int betRound;
-    int currentCallAmount;
-    int currentRaiseAmount; //Relevance?!? Redundant?
-    //Cards communityCards[];
+	int gameNumber;
+	int numActivePlayers;
+	int player;
+	int potMoney;
+	int bigBlind;
+	int smallBlind;
+	int playerTurn;
+	int betRound;
+	int currentCallAmount;
+	int currentRaiseAmount; //Relevance?!? Redundant?
+							//Cards communityCards[];
 
 public:
 
-    Game();
-    void printMainMenu();
-    void readRules();
-    void startBlinds();
-    void startPreFlop();
-    void startFlop();
-    void startTurn();
-    void startRiver();
-    void startShowdown();
-    void displayPlayerStats();
-    void playerAction(); //What is this?
-
-
+	Game();
+	void printMainMenu();
+	void readRules();
+	void startBlinds();
+	void startPreFlop();
+	void startFlop();
+	void startTurn();
+	void startRiver();
+	void startShowdown();
+	void displayPlayerStats();
+	void playerAction();
+	int getGameNumber();
+	void setGameNumber();
+	int getNumActivePlayers();
+	void setNumActivePlayers();
+	int getPlayer();
+	void setPlayer(int player);
+	int getPotMoney();
+	void setPotMoney(int potMoney);
+	int getBigBlind();
+	void setBigBlind(int bigBlind);
+	int getSmallBlind();
+	void setSmallBlind(int smallBlind);
+	int getPlayerTurn();
+	void setPlayerTurn(int playerTurn);
+	int getBetRound();
+	void setBetRound(int betRound);
+	int getCurrentCallAmount();
+	void setCurrentCallAmount(int currentCallAmount);
+	int getCurrentRaiseAmount();
+	void setCurrentRaiseAmount(int currentRaiseAmount);
 
 };
 
