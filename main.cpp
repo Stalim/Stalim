e6346b628e3d7848a27fae57dccf93015c54b9d6
perksofpaige <32183313+perksofpaige@users.noreply.github.com>
2018-04-18 16:49:11 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    string STRING;
+    ifstream openfile;
+    openfile.open("PokerRules.txt");
+    while (! openfile.eof()) // To get you all the lines.
+    {
+        getline(openfile, STRING); // Saves the line in STRING.
+        cout << STRING << "\n"; // Prints our STRING.
+    }
+    openfile.close();
+
+
     return 0;
 }
+
+
